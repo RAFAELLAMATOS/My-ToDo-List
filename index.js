050fe46070637abea252d998599eafd7efe3dfd7
RAFAELLAMATOS <rafaella.matos@gmail.com>
2023-11-05 09:21:24 -0300

    refactor: general improvements

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require("express"); 
+const path = require("path") 
+const routes = require("./routes/routes")
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const app = express(); 
+const port = 3000;  
 
-app.listen(3000);
+app.set("view engine", "ejs"); //set ejs view engine
+app.use(express.static(path.join(__dirname, "public"))) //use the css, js and all static files from public folder
+app.use(routes) //use the routes from the routes.js
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
